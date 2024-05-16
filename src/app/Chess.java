05b113706ae2de38c1da057b377e22a6f90e0b0d
Arthur Lage <arthurlage2006@gmail.com>
2024-05-16 19:01:24 -0300

    add show board to main file

--- src/app/Chess.java
+++ src/app/Chess.java
@@ -1,5 +1,11 @@
+import chess.ChessMatch;
+
 public class Chess {
   public static void main(String[] args) {
-    
+    System.out.println("\nCHESS\n");
+
+    ChessMatch chessMatch = new ChessMatch();
+
+    UI.printBoard(chessMatch.getPieces());
   }
 }
