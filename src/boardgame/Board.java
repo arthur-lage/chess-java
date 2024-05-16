542befb2e0312121a025e69c1babe4784f497af9
Arthur Lage <arthurlage2006@gmail.com>
2024-05-16 19:02:29 -0300

    add position existence checks and remove unnecessary setters

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,10 @@ public class Board {
   private Piece[][] pieces;
 
   public Board(int rows, int columns) {
+    if (rows < 1 || columns < 1) {
+      throw new BoardException("Error while creating board: The board must have at least 1 row and column.");
+    }
+
     this.rows = rows;
     this.columns = columns;
     this.pieces = new Piece[rows][columns];
@@ -15,35 +19,46 @@ public class Board {
     return rows;
   }
 
-  public void setRows(int rows) {
-    this.rows = rows;
-  }
-
   public int getColumns() {
     return columns;
   }
 
-  public void setColumns(int columns) {
-    this.columns = columns;
-  }
-
   public Piece piece(int row, int column) {
+    if (!positionExists(row, column)) {
+      throw new BoardException("This position does not exist.");
+    }
+
     return pieces[row][column];
   }
 
   public Piece piece(Position pos) {
+    if (!positionExists(pos)) {
+      throw new BoardException("This position does not exist.");
+    }
+
     return pieces[pos.getRow()][pos.getColumn()];
   }
 
   public void placePiece(Piece piece, Position pos) {
+    pieces[pos.getRow()][pos.getColumn()] = piece;
+    piece.position = pos;
   }
 
-  // public Piece removePiece(Position position) {
-  // }
+  public Piece removePiece(Position position) {
+    Piece piece = pieces[position.getRow()][position.getColumn()];
+    pieces[position.getRow()][position.getColumn()] = null;
+    return piece;
+  }
 
-  // public boolean positionExists(Position pos) {
-  // }
+  private boolean positionExists(int row, int column) {
+    return row >= 0 && rows < rows && column >= 0 && column < columns;
+  }
 
-  // public boolean isThereAnyPieces(Position pos) {
-  // }
+  public boolean positionExists(Position pos) {
+    return positionExists(pos.getRow(), pos.getColumn());
+  }
+
+  public boolean areThereAnyPieces(Position pos) {
+    return pieces[pos.getRow()][pos.getColumn()] != null;
+  }
 }
