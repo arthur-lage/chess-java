96e6fe41e20aeb9ee6e8fedc22cc490a39c3f00a
Arthur Lage <arthurlage2006@gmail.com>
2024-05-20 17:44:24 -0300

    fix: fix verification of position exists

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -55,7 +55,7 @@ public class Board {
   }
 
   private boolean positionExists(int row, int column) {
-    return row >= 0 && rows < rows && column >= 0 && column < columns;
+    return row >= 0 && row < rows && column >= 0 && column < columns;
   }
 
   public boolean positionExists(Position pos) {
